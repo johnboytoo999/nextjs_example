05e0bb9044b95927e6b92182495227ab454161e1
LAPTOP-DQB63RRG\johnb <johnboytoo999@gmail.com>
2023-02-16 17:48:13 +0800

    npm i @nestjs/config  to add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,14 +7,17 @@ import { CoffeeModule } from './coffee/coffee.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
-  imports: [CoffeeModule,CoffeeRatingModule,
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeeModule,
+    CoffeeRatingModule,
     TypeOrmModule.forRoot({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'pass123',
-    database: 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: +process.env.DATABASE_PORT,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     autoLoadEntities: true,
     synchronize:true,
   }),
