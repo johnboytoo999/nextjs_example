48e888a7f79ab19f27e360d9dd6d6bd676b735cf
LAPTOP-DQB63RRG\johnb <johnboytoo999@gmail.com>
2023-02-16 14:42:43 +0800

    transaction範例

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -4,9 +4,9 @@ import { CoffeeController } from './coffee.controller';
 import { CoffeeService } from './coffee.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
-
+import { Event } from '../events/entites/event.entity';
 @Module({
-    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
     controllers: [CoffeeController], 
     providers: [CoffeeService]})
 export class CoffeeModule {}
